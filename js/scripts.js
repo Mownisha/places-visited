b40306cfd6d553bf937cf2ca7bb05bca289506de
Mownisha <reach.mownisha@gmail.com>
2017-05-20 16:58:55 -0700

    added javascript and jquery to my webpage

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+function Place(name, landmarks, year, exp) {
+   this.placeName = name;
+   this.landmark = landmarks;
+   this.season = year;
+   this.notes = exp;
+}
+
+
+Place.prototype.outputString = function() {
+  return "During " + this.season + " I went to " + this.placeName;
+}
+
+
+$(document).ready(function() {
+  $("#form-one").submit(function(event) {
+    event.preventDefault();
+    var nameInput = $("#name").val();
+    var landmarkInput = $("#landmark").val();
+    var seasonInput = $("#season").val();
+    var notesInput = $("#notes").val();
+   if(!nameInput || !landmarkInput || !seasonInput || !notesInput) {
+      alert("Please enter all information");
+   }
+
+   else {
+      var newPlace = new Place (nameInput, landmarkInput, seasonInput, notesInput);
+
+      $("#result").append("<li><span class='place'>" + newPlace.outputString() + "</span></li>" )
+
+
+      $("#name").val("");
+      $("#landmark").val("");
+      $("#season").val("");
+      $("#notes").val("");
+
+      $(".place").last().click(function(){
+        $("#show-places").show();
+        $("#show-places h3").text(newPlace.placeName);
+        $("#name-output").text(newPlace.placeName);
+        $("#landmark-output").text(newPlace.landmark);
+        $("#season-output").text(newPlace.season);
+        $("#exp-output").text(newPlace.notes);
+      });
+    }
+
+  });
+
+
+});
